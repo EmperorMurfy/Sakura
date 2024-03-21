cdc53890122d11a08a775ef2cbadfd2452f9e7c9
Mason Z <110864180+EmperorMurfy@users.noreply.github.com>
2024-03-21 10:33:34 -0400

    Update consoleLog.js

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,1 +1,8 @@
-
+module.export =  (client) => {
+  console.log(`✅ ${c.user.tag} is online.`); 
+  client.user.setActivity({
+    name: 'Frying Pan ASMR',
+    type: ActivityType.Streaming,
+    url: 'https://www.youtube.com/watch?v=lrbOiYrMSPk',
+  });
+};
