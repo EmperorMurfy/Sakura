8dc1fd0d8de179e2fbeac1de8cb4aa02eb4b5ad5
Mason Z <110864180+EmperorMurfy@users.noreply.github.com>
2024-03-21 11:15:12 -0400

    Update eventHandler.js

--- src/handlers/eventHandler.js
+++ src/handlers/eventHandler.js
@@ -1,1 +1,24 @@
-module.export = ()
+const path = require ('path');
+const getAllFiles = require ('../utils/getAllFiles');
+
+module.export = (client) => {
+  const eventFolders = getAllFiles(path.join(__dirname, '..', 'events'), true);
+
+  // console.log(eventFolders); // 5:46
+
+  for (const eventFolder of eventFolders) {
+    const eventFiles = getAllFiles(eventFolder);
+    eventFiles.sort((a, b) => a > b );
+    // console.log(eventFiles); // 6:16
+
+    const eventName = eventFolder.replace(/\\/g, '/').split('/').pop();
+    // console.log(eventName); // 7:02
+
+    client.on(eventName, async(args) => {
+      for (const eventFile of eventFiles) {
+        const eventFunction = require(eventFile);
+        await eventFunction(client, args); 
+      }
+    }); 
+  }
+};
